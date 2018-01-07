62edcecd1396e841a5d510f5f9a084219d14e327
Martin Grigorov <Air@Martin.local>
2018-01-07 18:53:08 +0200

    Finish the challange, transfer, and withdraw functions

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,23 +4,34 @@ contract Remittance {
 
 	address public owner;
 	address public bob;
-	address public carol;
+	bytes32 private passHash;
 	uint public etherForTransfer;
+
+	mapping (address => uint ) public balances;
 	
 	event LogKillContract(address sender);
 	event LogEtherForTransferAdded(address sender, uint amount);
+	event LogTransferEther(address sender);
+	event LogWithdraw(address sender);
 
 	modifier onlyOwner() {
 		require(msg.sender == owner);
 		_;
 	}
+
+	modifier passwordsAvailable(bytes32 pass1, bytes32 pass2) {
+		require(pass1 != 0);
+		require(pass2 != 0);
+		_;
+	}
+
 	function createPassHash(bytes32 pass1, bytes32 pass2) 
 	public
 	constant
 	onlyOwner
-	returns(bytes32 passHash) 
+	passwordsAvailable(pass1,pass2)
 	{
-		return keccak256(pass1,pass2);
+		passHash = keccak256(pass1,pass2);
 	}
 
 	function challenge(bytes32 password) public payable onlyOwner {
@@ -29,9 +40,19 @@ contract Remittance {
 		LogEtherForTransferAdded(owner,msg.value);
 	}
 
-	function transferEther() {
+	function transferEther(bytes32 pass1, bytes32 pass2) public passwordsAvailable(pass1,pass2) {
+		require(passHash == keccak256(pass1,pass2));
+		balances[bob] += etherForTransfer;
+	}
 
-	};
+	function withdraw() public {
+        require(balances[msg.sender] > 0);
+        uint valueToWithdraw = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(valueToWithdraw);
+        
+        LogWithdraw(msg.sender);
+    }
 
 	function killSwitch() public onlyOwner {
         LogKillContract(owner);
